367aab0214e978b32a878e6f1eea59d2c460dff3
Nimesh Solanki <nimesh3536@gmail.com>
2017-06-22 12:48:44 +0530

    Add validations

--- server/server.js
+++ server/server.js
@@ -3,26 +3,50 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 mongoose.Promise = global.Promise;
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
+// var Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
+
+// var newTodo = new Todo({
+//     text: '    Hello mongoose   ',
+//     completed: false,
+//     // completedAt: new Date().getFullYear
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Todo successfully saved ', doc);
+// }, (err) => {
+//     console.log('Unable to save Todo ', err);
+// });
+
+var user = mongoose.model('Users', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     }
 });
 
-var newTodo = new Todo({
-    text: 'Hello mongoose',
-    completed: false,
-    // completedAt: new Date().getFullYear
+var newUser = new user({
+    email: 'anonymous17@hackernoon.com'
 });
 
-newTodo.save().then((doc) => {
-    console.log('Todo successfully saved ', doc);
+newUser.save().then((doc) => {
+    console.log('new user successfully saved ', doc);
 }, (err) => {
-    console.log('Unable to save Todo ', err);
+    console.log('unable to save user', e);
 });
